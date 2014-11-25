18d8172247c53b551b08b4afb8f1f350d8d90d5e
russphelan <russ.phelan@gmail.com>
2014-11-24 23:10:22 -0500

    finished baseball simulation in 3D. Needs to be refactored with more separate functions

--- Baseball Simulation/RK2_3D.m
+++ Baseball Simulation/RK2_3D.m
@@ -1,46 +1,71 @@
 % example of vectorization of RK2 for projectile motion of baseball 
-% including drag
+% including drag in 3D 
 
 %clear all; close all;       
 dt = input('Please enter the time step in seconds : ');
+vx0f = input('Please enter initial x velocity in mph: ');
+vy0f = input('Please enter initial y velocity in mph: ');
+vz0f = input('Please enter initial z velocity in mph: ');
+wHat = input('Please enter a unit vector in the direction of the axis of rotation using [x y z] notation: ');
+freq = input('Please enter frequency of rotation in Hz: ');
 
 g  = 9.8;                 % acceleration due to gravity in m/s^2
 m  = 0.145;                % baseball mass in kg
 Cd = 0.29;                 % drag coefficient Cd
-rho= 1.22;                 % density of air in kg/m^3
+rho= .9846;                 % density of air in kg/m^3
 D  = 7.45/100.0;           % baseball diameter in m
 R  = D/2;                  % baseball radius in m
 A  = pi*(R^2);             % baseball cross-sectional area in m^2
+Dm = .00065;               % coefficient for magnus force 
 
- x0 = 0.0;   y0 = 0.0;  z0 = 0.0;  % set initial position at origin
-vx0 = 25.0; vy0 = 25.0; vz0 = 50.0; % set initial velocity 
-
+ x0 = 0;   y0 = -.6;  z0 = 2.1336;  % set initial position at origin in feet
+vx0 = vx0f*.447; vy0 = vy0f*.447; vz0 = vz0f*.447; % set initial velocity in m/s
 r = [x0 y0 z0];               % make position row vector
-v = [vx0 vy0 vz0];             % make velocity row vector
+v = [vx0 vy0 vz0];             % make velocity row 
+wMag = freq*2*pi;
+w = wMag*wHat;
 
-t  = 0:dt:7;               % vector of times
+t  = 0:dt:5;               % vector of times
 N  = length(t);            % number of time steps
 
 rkeep = zeros(N,3);        % store baseball positions in this Nx2 array
                            % where first column has x position versus time
                            % second column has y position versus time
-                           % third colum has z pos versus time
-                           
+                           % t hird colum has z pos versus time
+vkeep = zeros(N,3); 
+tkeep = zeros(N,1);
 for i=1:N
    
-   a = myaccel_3D(v,Cd,rho,A,m,g);  % find acceleration from current pos, vel
+   a = myaccel_3D(v,Cd,rho,A,m,g,Dm,w);  % find acceleration from current pos, vel
    
    vmid = v + a*dt/2;            % find velocity at midpoint
-   amid = myaccel_3D(vmid,Cd,rho,A,m,g); % accel at midpoint depends on vmid
+   amid = myaccel_3D(vmid,Cd,rho,A,m,g,Dm,w); % accel at midpoint depends on vmid
    
    r = r + vmid*dt;
    v = v + amid*dt;
    
    rkeep(i,:)=r;    % now store the position vector for plotting
+   vkeep(i,:)=v;
+   tkeep(i) = dt*i;
    
 end
 
-stem3(rkeep(:,1),rkeep(:,2),rkeep(:,3),'LineWidth',.1);
-view(45,50);
+%converting from meters to feet
+rkeep = rkeep*3.2808;
+
+vkeep = vkeep*2.2369;
+
+%stem3(rkeep(:,1),rkeep(:,2),rkeep(:,3),'LineWidth',.1);
+%axis([0 60 -5 5 0 8]);
+%view(120,20);
+
+for i=1:length(rkeep)
+if(rkeep(i) < 60)
+    continue;
+else
+    break;
+end
+end
+i
 
 return;
--- Baseball Simulation/myaccel.m
+++ Baseball Simulation/myaccel.m
@@ -5,7 +5,7 @@ function a = myaccel(v,Cd,rho,A,m,g)
 vtemp = [v(1) v(2)];
 vmag  = norm(vtemp);
 
-Ftemp = [-0.5*Cd*rho*A*vmag*v(1), -0.5*Cd*rho*A*vmag*v(2) - m*g];
-a = Ftemp/m;
+Ftemp = [-0.5*Cd*rho*A*vmag*v(1), -0.5*Cd*rho*A*vmag*v(2) - m*g ];
+a = Ftemp/m; 
 
 return
